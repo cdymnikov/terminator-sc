586eed0b1e7c88ef6a469e656745e95f58c74965
cdymnikov <cdymnikov@gmail.com>
2015-02-02 03:15:11 +1300

    Joystick reader stubs

---
+++ Terminator.Tests/Input/Joystick/JoystickReaderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Terminator.Tests
+{
+    [TestClass]
+    public class DummyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
