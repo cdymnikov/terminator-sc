d0ea60d0ff43519560708dbbc3a7ac7a39227d1d
cdymnikov <cdymnikov@gmail.com>
2015-02-06 01:11:48 +1300

    Interface for mapping from device name to DirectX device GUIDs

---
+++ Terminator/Input/Joystick/DeviceGuidMapFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SharpDX.DirectInput;
+
+namespace Terminator.Input.Joystick
+{
+    public class DeviceGuidMapFactory : IDeviceGuidMapFactory
+    {
+        public IDictionary<string, IList<Guid>> CreateDeviceNameToGuidMapping()
+        {
+            throw new Exception("!");
+        }
+    }
+}
---
+++ Terminator/Input/Joystick/IDeviceGuidMapFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminator.Input.Joystick
+{
+    interface IDeviceGuidMapFactory
+    {
+        IDictionary<string, IList<Guid>> CreateDeviceNameToGuidMapping();
+    }
+}
