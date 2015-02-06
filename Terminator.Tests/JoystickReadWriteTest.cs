e30d0d7209657ebbe58337cbdbdb5eee9b214d59
cdymnikov <cdymnikov@gmail.com>
2015-02-06 17:11:35 +1300

    Structured elements for specifying joysticks in app.config

--- Terminator.Tests/JoystickReadWriteTest.cs
+++ Terminator.Tests/JoystickReadWriteTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using SharpDX.DirectInput;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Terminator.Input.Joystick;
+using Terminator.Configuration;
 
 namespace Terminator.Tests
 {
@@ -15,8 +16,10 @@ namespace Terminator.Tests
         [TestMethod]
         public void Axis_value_is_centered()
         {
-            var joystickProductName = ConfigurationManager.AppSettings["PhysicalJoystickTestDevice"].ToString();
-            var joystickProductNumber = int.Parse(ConfigurationManager.AppSettings["PhysicalJoystickTestDeviceNumber"]);
+            var id = (Devices)ConfigurationManager.GetSection("test/devices");
+
+            var joystickProductName = id.Joysticks["Physical Joystick"].ProductName;
+            var joystickProductNumber = id.Joysticks["Physical Joystick"].ProductNumber;
 
             var reader = _resolver.Resolve<IReaderFactory>().Create(new Identifier(joystickProductName, joystickProductNumber));
 
---
+++ Terminator/Configuration/Devices.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Configuration;
+using System.Xml;
+namespace Terminator.Configuration
+{
+    public class Devices : ConfigurationSection
+    {
+        [ConfigurationProperty("joysticks", IsRequired = true)]
+        public Joysticks Joysticks
+        {
+            get
+            {
+                return (Joysticks)this["joysticks"];
+            }
+            set
+            {
+                this["joysticks"] = value;
+            }
+        }
+    }
+
+    [ConfigurationCollection(typeof(JoystickIdentifier),
+    CollectionType = ConfigurationElementCollectionType.AddRemoveClearMap)]
+    public class Joysticks : ConfigurationElementCollection
+    {
+        #region Constructors
+        static Joysticks()
+        {
+            m_properties = new ConfigurationPropertyCollection();
+        }
+
+        public Joysticks()
+        {
+        }
+        #endregion
+
+        #region Fields
+        private static ConfigurationPropertyCollection m_properties;
+        #endregion
+
+        #region Properties
+        protected override ConfigurationPropertyCollection Properties
+        {
+            get { return m_properties; }
+        }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get { return ConfigurationElementCollectionType.AddRemoveClearMap; }
+        }
+        #endregion
+
+        #region Indexers
+        public JoystickIdentifier this[int index]
+        {
+            get { return (JoystickIdentifier)base.BaseGet(index); }
+            set
+            {
+                if (base.BaseGet(index) != null)
+                {
+                    base.BaseRemoveAt(index);
+                }
+                base.BaseAdd(index, value);
+            }
+        }
+
+        public JoystickIdentifier this[string name]
+        {
+            get { return (JoystickIdentifier)base.BaseGet(name); }
+        }
+        #endregion
+
+        #region Overrides
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new JoystickIdentifier();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return (element as JoystickIdentifier).Name;
+        }
+        #endregion
+    }
+
+
+
+    public class JoystickIdentifier : ConfigurationSection
+    {
+        [ConfigurationProperty("name", IsRequired = true)]
+        public string Name
+        {
+            get
+            {
+                return (string)this["name"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+
+        [ConfigurationProperty("productName", IsRequired = true)]
+        public string ProductName
+        {
+            get
+            {
+                return (string)this["productName"];
+            }
+            set
+            {
+                this["productName"] = value;
+            }
+        }
+
+        [ConfigurationProperty("productNumber", IsRequired = true)]
+        public int ProductNumber
+        {
+            get
+            {
+                return (int)this["productNumber"];
+            }
+            set
+            {
+                this["productNumber"] = value;
+            }
+        }
+    }
+}
